25ae5a714c35282a0eb1261faf1e23107dd119a4
rvbuijtenen <r.m.van.buijtenen@student.rug.nl>
2017-03-02 16:27:15 +0100

    created message and receiveMessageSocket classes

---
+++ net_computing_server/src/message_inbox/Message.java
@@ -0,0 +1,13 @@
+package message_inbox;
+
+public class Message {
+	private String message_body;
+	
+	public Message() {
+		this.message_body = "";
+	}
+	
+	public Message(String message_body) {
+		this.message_body = message_body;
+	}
+}
--- net_computing_server/src/message_inbox/MessageInbox.java
+++ net_computing_server/src/message_inbox/MessageInbox.java
@@ -1,5 +1,14 @@
 package message_inbox;
+import java.util.LinkedList;
 
-public class MessageInbox {
-
+public class MessageInbox extends LinkedList<Message> {
+	private ReceiveMessageSocket s;
+	public void enqueue(Message m) {
+		this.add(m);
+	}
+	
+	public Message dequeue() {
+		Message m = getFirst();
+		return m;
+	}
 }
