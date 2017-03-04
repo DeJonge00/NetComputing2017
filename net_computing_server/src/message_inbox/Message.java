5cfde3e3d541f91a7cd25becb90d9f11389b2cf7
rvbuijtenen <r.m.van.buijtenen@student.rug.nl>
2017-03-04 13:46:08 +0100

    added MessageInbox, connectionList and a library for hardware monitoring

--- net_computing_server/src/message_inbox/Message.java
+++ net_computing_server/src/message_inbox/Message.java
@@ -1,6 +1,7 @@
 package message_inbox;
+import java.io.Serializable;
 
-public class Message {
+public class Message implements Serializable {
 	private String message_body;
 	
 	public Message() {
@@ -9,5 +10,9 @@ public class Message {
 	
 	public Message(String message_body) {
 		this.message_body = message_body;
+	}
+	
+	public String get_message_body() {
+		return this.message_body;
 	}
 }
