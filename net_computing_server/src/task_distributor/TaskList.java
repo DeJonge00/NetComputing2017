91fec46c0f1165b86f071f9a321112404da1d07f
jannick <jannickboonstra@live.nl>
2017-04-01 18:48:55 +0200

    tasklist v1.0

---
+++ net_computing_server/src/task_distributor/TaskList.java
@@ -0,0 +1,62 @@
+package task_distributor;
+
+import java.util.ArrayList;
+
+public class TaskList {
+	public ArrayList<ActiveTask> activeTasks;
+	public ArrayList<FinishedTask> finishedTasks;
+	
+	public TaskList() {
+		activeTasks = new ArrayList<ActiveTask>();
+		finishedTasks = new ArrayList<FinishedTask>();
+	}
+	
+	public void insertTask(Task task) {
+		if (task instanceof FinishedTask) {
+			int index = find(finishedTasks, task.getUserId());
+			finishedTasks.add(index, (FinishedTask) task);
+		} else {
+			int index = find(activeTasks, task.getUserId());
+			activeTasks.add(index, (FinishedTask) task);
+		}
+	}
+	
+	public ArrayList<ActiveTask> getActiveTasksUser(int id) {
+		ArrayList<ActiveTask> usertasks = new ArrayList<ActiveTask>();
+		int i = find(activeTasks, id);
+		for (; activeTasks.get(i).getUserId() == id; i++) {
+			usertasks.add(activeTasks.get(i));
+			i++;
+		}
+		return usertasks;
+	}
+	
+	public ArrayList<FinishedTask> getFinishedTasksUser(int id) {
+		ArrayList<FinishedTask> usertasks = new ArrayList<FinishedTask>();
+		int i = find(finishedTasks, id);
+		for (; finishedTasks.get(i).getUserId() == id; i++) {
+			usertasks.add(finishedTasks.get(i));
+			i++;
+		}
+		return usertasks;
+	}
+	
+	/* Implements binary search to find the first occurence of a task with userid id */
+	private int find(ArrayList<? extends Task> list, int id) {
+		int l = 0, r = list.size() - 1, m = 0, currId;
+		while(l <= r) {
+			m = (l + r) / 2;
+			currId = list.get(m).getUserId();
+			if (currId < id) {
+				r = m - 1;
+			} else if (currId > id) {
+				l = m + 1;
+			} else if (l != m) {
+				r = m;
+			} else {
+				break;
+			}
+		}
+		return m;
+	}
+}
